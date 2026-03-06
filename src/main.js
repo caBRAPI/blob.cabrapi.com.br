073aa3c476cc49c3e2061300a4a2b104ae83cf3e
sebastianjnuwu <sebastianjnuwu@gmail.com>
2026-03-05 21:56:20 -0300

    feat: initialize project with Express, Prisma, and custom logger
    - Added package.json with scripts and dependencies
    - Created Prisma configuration and schema for Blob model
    - Implemented custom logger using Winston and DailyRotateFile
    - Set up main Express server with middleware for security and logging
    - Added health check endpoint

---
+++ src/main.js
@@ -0,0 +1,78 @@
+import body from "body-parser";
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { v7 as uuidv7 } from "uuid";
+import { logger } from "#functions/logger";
+import "dotenv/config";
+
+const app = express();
+
+app.set("trust proxy", 1);
+app.disable("x-powered-by");
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "'unsafe-inline'"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                imgSrc: ["'self'", "data:", "https:"],
+                connectSrc: ["'self'"],
+                fontSrc: ["'self'"],
+                objectSrc: ["'none'"],
+                mediaSrc: ["'self'"],
+                frameSrc: ["'none'"],
+            },
+        },
+        hsts: {
+            maxAge: 31536000,
+            includeSubDomains: true,
+            preload: true,
+        },
+        referrerPolicy: {
+            policy: "strict-origin-when-cross-origin",
+        },
+    }),
+);
+app.use(compression());
+app.use(body.json({ limit: "250mb" }));
+app.use(
+    body.urlencoded({
+        limit: "250mb",
+        extended: true,
+    }),
+);
+
+app.use(
+    morgan(
+        `(ID: ${uuidv7()}) - ` +
+        ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" (:response-time ms)',
+        {
+            stream: { write: (x) => logger.info(x.trim(), { type: "http" }) },
+        },
+    ),
+);
+
+app.get("/", (_, res) => {
+    return res.send("OK");
+});
+
+app.get("/health", (_, res) => {
+    return res.send("OK");
+});
+
+app.listen(process.env.PORT ?? 3000, process.env.HOST ?? "0.0.0.0", () => {
+    return logger.debug(
+        `Server running: <green>http://${process.env.HOST ?? "0.0.0.0"}:${process.env.PORT ?? 3000}</green>`,
+    );
+});
